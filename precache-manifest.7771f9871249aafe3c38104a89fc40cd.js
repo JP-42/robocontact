2c336d115163ac8d986b9c35dcab4baedb6de805
John Pete <johnpete@Johns-MacBook-Pro.local>
2020-07-09 10:33:30 +0200

    Updates

---
+++ precache-manifest.7771f9871249aafe3c38104a89fc40cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74d8098f157da01974d83f52e57778e5",
+    "url": "/robocontact/index.html"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robocontact/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "70ca1df64cc9a10b3f5e",
+    "url": "/robocontact/static/css/main.36068543.chunk.css"
+  },
+  {
+    "revision": "abcf402c9619777cd68e",
+    "url": "/robocontact/static/js/2.cab51068.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robocontact/static/js/2.cab51068.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70ca1df64cc9a10b3f5e",
+    "url": "/robocontact/static/js/main.4b0f592e.chunk.js"
+  },
+  {
+    "revision": "2fcf79a0b38b4072c5fd",
+    "url": "/robocontact/static/js/runtime-main.511f5b25.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robocontact/static/media/SEGA.12a3db8b.woff"
+  }
+]);
